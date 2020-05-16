69fe055456d2c5bfb2ba56124486c409356f3f21
AlexejheroYTB <alexejhero@gmail.com>
2020-05-17 01:36:29 +0300

    Update ziraport.ts

--- src/commands/developer/ziraport.ts
+++ src/commands/developer/ziraport.ts
@@ -4,7 +4,6 @@ import * as data from "../../data";
 import Command from "../../data_types/command";
 import CommandPermission from "../../data_types/command_permission";
 import ModIdea from "../../data_types/mod_idea";
-import ModIdeaStatus from "../../data_types/mod_idea_status";
 import * as embeds from "../../embeds";
 import * as parser from "../../parser";
 import * as util from "../../util";
@@ -24,31 +23,25 @@ export default class implements Command {
     if (!oldReleased) return embeds.error(message, "Invalid arguments. Expected a text channel id as the second parameter");
 
     const newList = parser.textChannel(args[2]);
-    if (!newList) return embeds.error(message, "Invalid arguments. Expected a text channel id as the fourth parameter");
+    if (!newList) return embeds.error(message, "Invalid arguments. Expected a text channel id as the third parameter");
 
     const newReleased = parser.textChannel(args[3]);
-    if (!newReleased) return embeds.error(message, "Invalid arguments. Expected a text channel id as the fifth parameter");
+    if (!newReleased) return embeds.error(message, "Invalid arguments. Expected a text channel id as the fourth parameter");
 
     var oldListMessages = await util.getAllMessages(oldList);
     for (var msg of oldListMessages) {
-      var idea = await this.createNormalZiraIdea(msg);
+      var idea = await this.createZiraIdea(msg);
       idea.send(newList.id, true, true);
     }
 
     var oldReleasedMessages = await util.getAllMessages(oldReleased);
     for (var msg of oldReleasedMessages) {
-      var idea = await this.createNormalZiraIdea(msg);
+      var idea = await this.createZiraIdea(msg);
       idea.send(newReleased.id, true, false);
     }
-
-    // var oldRemovedMessages = await util.getAllMessages(oldRemoved);
-    // for (var msg of oldRemovedMessages) {
-    //   var idea = await this.createRemovedZiraIdea(msg);
-    //   idea.send(newRemoved.id, true, false);
-    // }
   }
 
-  private async createNormalZiraIdea(message: Discord.Message) {
+  private async createZiraIdea(message: Discord.Message) {
     var last = ModIdea.getLastFileId();
     var ideas = data.read("mod_ideas/" + last, []) as ModIdea[];
 
@@ -68,27 +61,4 @@ export default class implements Command {
 
     return idea;
   }
-
-  // private async createRemovedZiraIdea(message: Discord.Message) {
-  //   var last = ModIdea.getLastFileId();
-  //   var ideas = data.read("mod_ideas/" + last, []) as ModIdea[];
-
-  //   if (ideas.length === 100) {
-  //     ideas = [];
-  //     last++;
-  //   }
-
-  //   const embed = message.embeds[0];
-  //   const id = (embed.author?.name?.match(/\d{13,}/g) ?? [undefined])[0];
-  //   const idea = new ModIdea(ModIdea.getNextID(), embed.description ?? embed.fields[0].value, id ?? message.author.id, undefined);
-  //   idea.status = ModIdeaStatus.Removed;
-  //   idea.lastActor = "493359366381240341";
-  //   idea.time = embed.timestamp ?? 0;
-  //   idea.comment = embed.description ? (embed.fields.length > 0 ? embed.fields[0].value : "") : (embed.fields.length > 1 ? embed.fields[1].value : "");
-
-  //   ideas.push(idea);
-  //   data.write("mod_ideas/" + last, ideas);
-
-  //   return idea;
-  // }
 }
