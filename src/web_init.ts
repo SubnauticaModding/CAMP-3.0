f2300d7d85eae943fa1502d48a947777e2a983b0
AlexejheroYTB <alexejhero@gmail.com>
2020-05-30 01:29:28 +0300

    Keepalive

--- src/web_init.ts
+++ src/web_init.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 console.log("Starting web server...");
 const web = express();
+web.use(require("express-glitch-keepalive"));
 web.all("*", (_, res) => {
   res.redirect("https://discord.gg/UpWuWwq");
 });
