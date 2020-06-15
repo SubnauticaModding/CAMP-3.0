5a17aff6f6dcb0e4e87fa2f43d9a1e1cb5b3f4b3
Glitch (camp-3-0) <none>
2020-06-15 16:55:21 +0000

    Glitch Update

--- src/data_types/mod_idea.ts
+++ src/data_types/mod_idea.ts
@@ -361,7 +361,7 @@ export default class ModIdea {
       total++;
     }
 
-    var topIdeas = [...ideas].sort((a, b) => a.rating.likes.length - a.rating.dislikes.length - b.rating.likes.length + b.rating.dislikes.length).slice(0, 10);
+    var topIdeas = [...ideas].sort((a, b) => - a.rating.likes.length + a.rating.dislikes.length + b.rating.likes.length - b.rating.dislikes.length).slice(0, 10);
 
     const embed = new Discord.MessageEmbed();
     embed.setColor("BLUE");
